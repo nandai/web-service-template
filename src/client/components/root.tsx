b43650347fea8b43b83fdedd4871945e49fefcc0
nandai <coffeemam@gmail.com>
2017-05-06 23:12:55 +0900

    スクロール位置を保持するため非アクティブなビューは破棄せず非表示にするように改修

--- src/client/components/root.tsx
+++ src/client/components/root.tsx
@@ -12,9 +12,10 @@ interface RootProps
 
 interface RootState
 {
-    view     : JSX.Element;
-    nextView : JSX.Element;
-    fade     : boolean;
+    views?       : JSX.Element[];
+    currentView? : JSX.Element;
+    nextView?    : JSX.Element;
+    fade?        : boolean;
 }
 
 export default class Root extends React.Component<RootProps, RootState>
@@ -28,9 +29,10 @@ export default class Root extends React.Component<RootProps, RootState>
 
         this.state =
         {
-            view:     props.view,
-            nextView: null,
-            fade:     false
+            views:      [props.view],
+            currentView: props.view,
+            nextView:    null,
+            fade:        false
         };
 
         this.onTransitionEnd = this.onTransitionEnd.bind(this);
@@ -47,10 +49,21 @@ export default class Root extends React.Component<RootProps, RootState>
         if (state.fade)
             className += ' fade';
 
+        // スクロール位置を保持するため、非アクティブなビューは破棄せず非表示にしておく
+        const elements = state.views.map((view, i) =>
+        {
+            const style =
+            {
+                display: (view.type === state.currentView.type ? 'flex' : 'none'),
+                flexGrow: 1
+            };
+            return <div key={i} style={style}>{view}</div>
+        });
+
         return (
             <div className="root" tabIndex={0}>
                 <div className={className} ref="root" onTransitionEnd={this.onTransitionEnd}>
-                    {this.state.view}
+                    {elements}
                 </div>
             </div>
         );
@@ -61,18 +74,24 @@ export default class Root extends React.Component<RootProps, RootState>
      */
     componentWillReceiveProps(nextProps : RootProps)
     {
-        if (this.state.view.type === nextProps.view.type || nextProps.effect === undefined || nextProps.effect === 'none')
+        if (this.state.currentView.type === nextProps.view.type || nextProps.effect === undefined || nextProps.effect === 'none')
         {
-            this.setState({view:nextProps.view});
+            const newViews = this.addOrReplaceView(nextProps.view);
+            const newState : RootState =
+            {
+                views:       newViews,
+                currentView: nextProps.view
+            };
+            this.setState(newState);
         }
         else
         {
-            const state =
+            const newState : RootState =
             {
                 nextView: nextProps.view,
                 fade:     true
             };
-            this.setState(state);
+            this.setState(newState);
         }
     }
 
@@ -81,18 +100,48 @@ export default class Root extends React.Component<RootProps, RootState>
      */
     onTransitionEnd(e : React.TransitionEvent<Element>)
     {
+        const {state} = this;
+        const {views} = state;
         const el = ReactDOM.findDOMNode(this.refs['root']);
 
-        if (el === e.target && this.state.nextView)
+        if (el === e.target && state.nextView)
         {
-            const state =
+            const newViews = this.addOrReplaceView(state.nextView);
+            const newState : RootState =
             {
-                view:     this.state.nextView,
-                nextView: null
+                views:       newViews,
+                currentView: state.nextView,
+                nextView:    null
             };
-            this.setState(state);
+            this.setState(newState);
 
+            // ワンテンポずらす（そうしないとエフェクトが発動しないため）
             setTimeout(() => this.setState({fade:false}), 0);
         }
+    }
+
+    /**
+     * ビューを追加または置き換え
+     */
+    private addOrReplaceView(addView : JSX.Element) : JSX.Element[]
+    {
+        const {state} = this;
+        const newViews = Object.assign([], state.views);
+        let exists = false;
+
+        for (let i = 0; i < newViews.length; i++)
+        {
+            if (newViews[i].type === addView.type)
+            {
+                newViews[i] = addView;
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists === false)
+            newViews.push(addView);
+
+        return newViews;
     }
 }
