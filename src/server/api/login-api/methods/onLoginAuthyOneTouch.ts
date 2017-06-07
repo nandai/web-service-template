274d7af3f05837ac78509b4a43cfddae0b9f5f43
nandai <coffeemam@gmail.com>
2017-06-08 00:41:20 +0900

    Authy OneTouchの時にログイン履歴を記録していなかったので修正

--- src/server/api/login-api/methods/onLoginAuthyOneTouch.ts
+++ src/server/api/login-api/methods/onLoginAuthyOneTouch.ts
@@ -1,10 +1,11 @@
 /**
  * (C) 2016-2017 printf.jp
  */
-import {Response}              from 'libs/response';
-import Authy                   from 'server/libs/authy';
-import Utils                   from 'server/libs/utils';
-import SessionModel, {Session} from 'server/models/session-model';
+import {Response}                        from 'libs/response';
+import Authy                             from 'server/libs/authy';
+import Utils                             from 'server/libs/utils';
+import LoginHistoryModel, {LoginHistory} from 'server/models/login-history-model';
+import SessionModel, {Session}           from 'server/models/session-model';
 
 import express = require('express');
 import slog =    require('server/slog');
@@ -30,9 +31,17 @@ export async function onLoginAuthyOneTouch(req : express.Request, res : express.
 
                 if (approval)
                 {
+                    // セッション更新
                     session.sms_id =     null;
+                    session.sms_code =   null;
                     session.authy_uuid = null;
                     await SessionModel.update(session);
+
+                    // ログイン履歴作成
+                    const loginHistory = new LoginHistory();
+                    loginHistory.account_id = session.account_id;
+                    loginHistory.device = req.headers['user-agent'];
+                    await LoginHistoryModel.add(loginHistory);
                 }
             }
 
